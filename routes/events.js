4993e2cf62cdc9c504c5ce2acfad38d8e5bd06a1
alejandrom95 <alejandrom060595@gmail.com>
2017-03-12 21:50:06 -0700

    completed events backend
    Finished backend functionality for the sprint 2 backend for events and
    added basic forms for testing in the o_events.hjs page which is the
    page where the owner of an event manages the event. It still needs work
    on the front end.

--- routes/events.js
+++ routes/events.js
@@ -79,10 +79,65 @@ router
 
   })
   .get("/event/:event_id", loginRequired, (req, res, next) => {
-    res.render("event", {
-      event_id: req.params.event_id
-    })
+    const event_id = parseInt(req.params.event_id);
+    db("events")
+      .where("event_id", event_id)
+      .then((events) => {
+        if(events.length > 0) {
+          res.render("event", {
+            event_id: events[0].event_id,
+            event_name: events[0].event_name,
+            owner_email: events[0].owner_email,
+            description: events[0].description,
+            status: events[0].status,
+            location: events[0].location,
+            zipcode: events[0].zipcode,
+            date_time: events[0].date_time
+          })
+        }
+        else {
+          res.redirect('/')
+        }
+        
+      })
+    // res.render("event", {
+    //   event_id: req.params.event_id
+    // })
   })
+  .get("/o_event/:event_id", loginRequired, (req, res, next) => {
+    const event_id = parseInt(req.params.event_id);
+    db("events")
+      .where("event_id", event_id)
+      .where("owner_email", req.user.email)
+      .then((events) => {
+        db("volunteers")
+        .where("event_id", event_id)
+        .then((volunteers) => {
+          res.render("o_event", {
+            volunteers: volunteers,
+            event_id: events[0].event_id,
+            event_name: events[0].event_name,
+            owner_email: events[0].owner_email,
+            description: events[0].description,
+            status: events[0].status,
+            location: events[0].location,
+            zipcode: events[0].zipcode,
+            date_time: events[0].date_time
+          })
+        })
+      })
+    // res.render("event", {
+    //   event_id: req.params.event_id
+    // })
+  })
+  // const event_id = parseInt(req.params.event_id);
+    // db("volunteers")
+    //   .where("event_id", event_id)
+    //   .then((volunteers) => {
+    //     res.render("event", {
+    //       volunteers: volunteers
+    //     })
+    //   })
   .post("/request_to_join_event", loginRequired, (req, res, next) => {
     const event_id = parseInt(req.body.event_id);
 
@@ -93,21 +148,108 @@ router
       .where("event_id", event_id)
       .then((email) => {
         owner_email = email[0].owner_email
+        var newVolunteerRequest = {
+          event_id: event_id,
+          owner_email: owner_email,
+          participant_email: req.user.email,
+          status: false
+        };
+        // res.render(newEvent);
+        db("volunteers")
+          .insert(newVolunteerRequest)
+          .then((ids) => {
+            // res.send(ids)
+            res.redirect("/")
+        }, next)
       })
+  })
+  .post("/accept_volunteer", loginRequired, (req, res, next) => {
+    const event_id = parseInt(req.body.event_id);
+    const participant_email = req.body.participant_email;
+
+    db("events")
+      .where("event_id", event_id)
+      .then((events) => {
+        if(req.user.email === events[0].owner_email) {
+          db("volunteers")
+            .where("event_id", event_id)
+            .where("participant_email", participant_email)
+            .update(
+            {
+              status: true
+            })
+            .then((result) => {
+              if(result === 0) {
+                return res.send(400)
+              }
+              // res.send(200)
+              var retDir = "/o_event/" + event_id;
+              res.redirect(retDir)
+          }, next)
+        }
+        else {
+          res.redirect('/')
+        }
+        
+      })
+
+      //alter get for evnet, make sure you display all people who have applied for event during event get method: line 81
     
-    var newVolunteerRequest = {
-      event_id: event_id,
-      owner_email: owner_email,
-      participant_email: req.user.email,
-      status: false
-    };
-    // res.render(newEvent);
-    db("volunteers")
-      .insert(newVolunteerRequest)
-      .then((ids) => {
-        // res.send(ids)
-        res.redirect("/")
-    }, next)
+  })
+  .post("/deny_volunteer", loginRequired, (req, res, next) => {
+    const event_id = parseInt(req.body.event_id);
+    const participant_email = req.body.participant_email;
+
+
+    db("events")
+      .where("event_id", event_id)
+      .then((events) => {
+        if(req.user.email === events[0].owner_email) {
+          db("volunteers")
+            .where("event_id", event_id)
+            .where("participant_email", participant_email)
+            .update(
+            {
+              status: false
+            })
+            .then((result) => {
+              if(result === 0) {
+                return res.send(400)
+              }
+              // res.send(200)
+              var retDir = "/o_event/" + event_id;
+              res.redirect(retDir)
+          }, next)
+        }
+        else {
+          res.redirect('/')
+        }
+        
+      })
+  })
+  .post("/modify_event", loginRequired, (req, res, next) => {
+    const event_id = parseInt(req.body.event_id);
+    db("events")
+      .where("event_id", event_id)
+      .where("owner_email", req.user.email)
+      .update(
+      {
+        event_name: req.body.event_name,
+        owner_email: req.user.email,
+        description: req.body.event_description,
+        status: 1,
+        location: req.body.event_location,
+        zipcode: req.body.event_zipcode,
+        date_time: req.body.event_date_time
+      })
+      .then((result) => {
+        if(result === 0) {
+          return res.send(400)
+        }
+        // res.send(200)
+        var retDir = "/o_event/" + event_id;
+        res.redirect(retDir)
+      }, next)
   })
 
 module.exports = router
